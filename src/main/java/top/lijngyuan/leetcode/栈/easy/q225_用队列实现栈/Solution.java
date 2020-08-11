8b469a523dbc7fcb374daacfce9c2f2c03eca828
kangjinghang <34730988+kangjinghang@users.noreply.github.com>
2020-08-11 14:00:16 +0800

    q225 solution

---
+++ src/main/java/top/lijngyuan/leetcode/栈/easy/q225_用队列实现栈/Solution.java
@@ -0,0 +1,11 @@
+package top.lijngyuan.leetcode.栈.easy.q225_用队列实现栈;
+
+/**
+ * TODO
+ *
+ * @author <a href="kangjinghang@gmail.com">kangjinghang</a>
+ * @date 2020-08-11
+ * @since 1.0.0
+ */
+public class Solution {
+}
