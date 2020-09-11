4d59c5c4f9a53380692e05945435a412ee766a69
kangjinghang <34730988+kangjinghang@users.noreply.github.com>
2020-09-11 16:16:56 +0800

    q70 solution

---
+++ src/main/java/top/lijngyuan/leetcode/动态规划/easy/q70_爬楼梯/Solution.java
@@ -0,0 +1,69 @@
+package top.lijngyuan.leetcode.动态规划.easy.q70_爬楼梯;
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+// 注意：给定 n 是一个正整数。
+//
+// 示例 1：
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶
+//
+// 示例 2：
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+//
+// Related Topics 动态规划
+// 👍 1228 👎 0
+
+/**
+ * Solution
+ *
+ * @author <a href="kangjinghang@gmail.com">kangjinghang</a>
+ * @date 2020-09-11
+ * @since 1.0.0
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(1));
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(5));
+    }
+
+    public static int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+}
