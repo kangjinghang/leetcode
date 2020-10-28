9e3241e5a9472f29f08a30b93053ff319cbe1024
kangjh <kangjinghang@gmail.com>
2020-10-28 18:32:58 +0800

    q98 solution

---
+++ src/main/java/top/lijngyuan/leetcode/二叉树/medium/q98_验证二叉搜索树/Solution.java
@@ -0,0 +1,86 @@
+package top.lijngyuan.leetcode.二叉树.medium.q98_验证二叉搜索树;
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+//
+// 假设一个二叉搜索树具有如下特征：
+//
+//
+// 节点的左子树只包含小于当前节点的数。
+// 节点的右子树只包含大于当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+//
+//
+// 示例 1:
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+//
+//
+// 示例 2:
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+//
+// Related Topics 树 深度优先搜索
+// 👍 817 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Solution
+ *
+ * @author <a href="kangjh@shukun.net">kangjinghang</a>
+ * @date 2020-10-28
+ * @since 1.0.0
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(4);
+        root.left = node1;
+        root.right = node4;
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(6);
+        node4.left = node3;
+        node4.right = node6;
+        System.out.println(isValidBST(root));
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    // 引入上界与下界，用以保存之前的节点中出现的最大值与最小值
+    public static boolean isValidBST(TreeNode root, long max, long min) {
+        if (root == null) {
+            return true;
+        }
+        if (min >= root.val || max <= root.val) {
+            return false;
+        }
+        return isValidBST(root.left, root.val, min) && isValidBST(root.right, max, root.val);
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
