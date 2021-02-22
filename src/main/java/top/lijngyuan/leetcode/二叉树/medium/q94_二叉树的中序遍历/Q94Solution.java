378617071a5c050527d663a794678795c129d425
kangjh <kangjinghang@gmail.com>
2021-02-22 15:46:06 +0800

    q94 solution

---
+++ src/main/java/top/lijngyuan/leetcode/二叉树/medium/q94_二叉树的中序遍历/Q94Solution.java
@@ -0,0 +1,149 @@
+package top.lijngyuan.leetcode.二叉树.medium.q94_二叉树的中序遍历;
+
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+// 示例 4：
+//
+//
+//输入：root = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 5：
+//
+//
+//输入：root = [1,null,2]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 哈希表
+// 👍 862 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * q94 solution
+ *
+ * @author <a href="kangjh@shukun.net">kangjinghang</a>
+ * @date 2021-02-22
+ * @since 1.0.0
+ */
+public class Q94Solution {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        root.right = node2;
+        node2.left = node3;
+        // 1,3,2
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        // 中序：左 中 右
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                // 先压中，使用时后使用中
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+        return list;
+    }
+
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.addAll(inorderTraversal2(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal2(root.right));
+        return list;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
